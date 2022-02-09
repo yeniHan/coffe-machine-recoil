c217fbf70b0ab48c58398215927bf4e41f51d1f7
Yena Han <vovv760@gmail.com>
2022-02-09 20:01:33 +0900

    readme

--- src/Pages/CouponPage/index.tsx
+++ src/Pages/CouponPage/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 import {useRecoilValue, useRecoilState} from "recoil";
-import {couponsState, paymentState, selectedCouponState} from "../../recoil";
+import {couponsState, selectedCouponState} from "../../recoil";
 import NextButton from "../../Components/NextButton";
 import BackButton from "../../Components/Backbutton";
 import SelectedMenuList from "../../Components/SelectedMenuList";
@@ -21,7 +21,7 @@ const CouponList = styled.div`
 
 const Coupon = styled.div`
   width: 30%;
-  height: 150px;
+  height: 142px;
   border-radius: 5px;
   border: 1px solid black;
   cursor: pointer;
@@ -34,11 +34,11 @@ const Coupon = styled.div`
 const CouponPage = () => {
     const coupons = useRecoilValue(couponsState)
     const [selectedCoupon, setSelectedCoupon] = useRecoilState(selectedCouponState)
-    const payment = useRecoilValue(paymentState)
 
     return (
         <Wrapper>
-            <h1>쿠폰을 선택하세요.</h1>
+            <h2>쿠폰을 선택하세요.</h2>
+            <h5>*Takeout 메뉴는 쿠폰 적용 불가</h5>
             <CouponList>
                 {coupons?.map((v) => {
                     const isSelected = v.id === selectedCoupon?.id
